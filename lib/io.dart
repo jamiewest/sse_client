555689d5d9fbf65b5ea9368833f61f59799ff5ce
Michael Söderman <micke@kattalo.com>
2021-07-02 12:29:41 +0200

    Changing exceptions to custom type so that we can catch them further up in the call chain

--- lib/io.dart
+++ lib/io.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:http/http.dart';
+import 'package:http/io_client.dart';
 import 'package:sse_client/src/event_source_transformer.dart';
 import 'package:sse_client/src/sse_client.dart';
 
@@ -20,30 +22,86 @@ class IOSseClient extends SseClient {
 
   factory IOSseClient.connect(Uri uri) {
     late StreamController<String?> incomingController;
-    final client = Client();
+    final client = ClientWithCustomExceptionType();
 
     incomingController = StreamController<String?>.broadcast(onListen: () {
       final request = Request('GET', uri)
         ..headers['Accept'] = 'text/event-stream';
 
-      try {
-        client.send(request).then((response) {
-          if (response.statusCode == 200) {
-            response.stream.transform(EventSourceTransformer()).listen((event) {
-              incomingController.sink.add(event.data);
-            });
-          } else {
-            incomingController
-                .addError(Exception('Failed to connect to ${uri.toString()}'));
-          }
-        });
-      } catch (e) {
-        throw SseClientException('SseClientException, inner exception: $e');
-      }
+      client.send(request).then((response) {
+        if (response.statusCode == 200) {
+          response.stream.transform(EventSourceTransformer()).listen((event) {
+            incomingController.sink.add(event.data);
+          });
+        } else {
+          incomingController.addError(
+              SseClientException('Failed to connect to ${uri.toString()}'));
+        }
+      });
     }, onCancel: () {
       incomingController.close();
     });
 
     return IOSseClient(incomingController.stream);
+  }
+}
+
+class ClientWithCustomExceptionType extends BaseClient {
+  /// The underlying `dart:io` HTTP client.
+  final HttpClient _inner;
+
+  ClientWithCustomExceptionType([HttpClient? inner])
+      : _inner = inner ?? HttpClient();
+
+  /// Sends an HTTP request and asynchronously returns the response.
+  @override
+  Future<IOStreamedResponse> send(BaseRequest request) async {
+    var stream = request.finalize();
+
+    try {
+      var ioRequest = (await _inner.openUrl(request.method, request.url))
+        ..followRedirects = request.followRedirects
+        ..maxRedirects = request.maxRedirects
+        ..contentLength = (request.contentLength ?? -1)
+        ..persistentConnection = request.persistentConnection;
+      request.headers.forEach((name, value) {
+        ioRequest.headers.set(name, value);
+      });
+
+      var response = await stream.pipe(ioRequest) as HttpClientResponse;
+
+      var headers = <String, String>{};
+      response.headers.forEach((key, values) {
+        headers[key] = values.join(',');
+      });
+
+      return IOStreamedResponse(
+          response.handleError((error) {
+            final httpException = error as HttpException;
+            throw SseClientException('handleError: ${httpException.message}');
+          }, test: (error) => error is HttpException),
+          response.statusCode,
+          contentLength:
+              response.contentLength == -1 ? null : response.contentLength,
+          request: request,
+          headers: headers,
+          isRedirect: response.isRedirect,
+          persistentConnection: response.persistentConnection,
+          reasonPhrase: response.reasonPhrase,
+          inner: response);
+    } on HttpException catch (error) {
+      throw SseClientException('HttpException: ${error.message}');
+    } catch (e) {
+      throw SseClientException('Exception: type: ${e.runtimeType}, e = $e');
+    }
+  }
+
+  /// Closes the client.
+  ///
+  /// Terminates all active connections. If a client remains unclosed, the Dart
+  /// process may not terminate.
+  @override
+  void close() {
+    _inner.close(force: true);
   }
 }
