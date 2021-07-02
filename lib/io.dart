b38d8724c229e48bd76b143500f40b40859dfa49
Michael Söderman <micke@kattalo.com>
2021-07-02 11:08:36 +0200

    Implementing custom exception inside IOSseClient

--- lib/io.dart
+++ lib/io.dart
@@ -4,6 +4,17 @@ import 'package:http/http.dart';
 import 'package:sse_client/src/event_source_transformer.dart';
 import 'package:sse_client/src/sse_client.dart';
 
+class SseClientException implements Exception {
+  final String message;
+
+  SseClientException(this.message);
+
+  @override
+  String toString() {
+    return 'SseClientException: $message';
+  }
+}
+
 class IOSseClient extends SseClient {
   IOSseClient(Stream stream) : super(stream: stream);
 
@@ -12,19 +23,23 @@ class IOSseClient extends SseClient {
     final client = Client();
 
     incomingController = StreamController<String?>.broadcast(onListen: () {
-      var request = Request('GET', uri)
+      final request = Request('GET', uri)
         ..headers['Accept'] = 'text/event-stream';
 
-      client.send(request).then((response) {
-        if (response.statusCode == 200) {
-          response.stream.transform(EventSourceTransformer()).listen((event) {
-            incomingController.sink.add(event.data);
-          });
-        } else {
-          incomingController
-              .addError(Exception('Failed to connect to ${uri.toString()}'));
-        }
-      });
+      try {
+        client.send(request).then((response) {
+          if (response.statusCode == 200) {
+            response.stream.transform(EventSourceTransformer()).listen((event) {
+              incomingController.sink.add(event.data);
+            });
+          } else {
+            incomingController
+                .addError(Exception('Failed to connect to ${uri.toString()}'));
+          }
+        });
+      } catch (e) {
+        throw SseClientException('SseClientException, inner exception: $e');
+      }
     }, onCancel: () {
       incomingController.close();
     });
